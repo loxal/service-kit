63fc2dc572e72d30c26b53d63c377477e5205bb0
Alex <alexander.orlov@loxal.net>
2015-01-05 18:57:05 +0100

    port additional Java code to Kotlin

---
+++ src/main/kotlin/net/loxal/soa/restkit/App.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2015 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ */
+
+package net.loxal.soa.restkit
+
+import javax.ws.rs.ApplicationPath
+import org.glassfish.jersey.server.ResourceConfig
+
+ApplicationPath("")
+public class App : ResourceConfig() {
+    {
+        packages(javaClass.getPackage().getName())
+    }
+}
