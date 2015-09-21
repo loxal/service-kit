cc8d7f797232fb8835d8170f8a1b28f620cc513b
Alex <alexander.orlov@loxal.net>
2015-09-21 15:32:41 +0200

    adjust deployment descriptor

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 
 # Switch to Java 8 JDK
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
-mvn clean package cf:push -P cf-aws-prod
+mvn clean package cf:push -P cf-aws-stage
 # mvn clean package appengine:update # GAE (managed)
 # mvn clean validate gcloud:deploy # gcloud (managed / unmanaged)
