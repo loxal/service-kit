2dbf9ceee29d8d687be7008f767dff24efd1cf25
Alex <alexander.orlov@loxal.net>
2015-09-21 20:33:57 +0200

    add user access management endpoint for AppDirect

---
+++ src/main/kotlin/net/loxal/soa/restkit/endpoint/appdirect/AccessResource.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ */
+
+package net.loxal.soa.restkit.endpoint.appdirect
+
+import net.loxal.soa.restkit.client.RepositoryClient
+import net.loxal.soa.restkit.endpoint.Endpoint
+import net.loxal.soa.restkit.endpoint.SubscriptionResource
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.container.AsyncResponse
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.container.Suspended
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+@Path(AccessResource.RESOURCE_PATH)
+class AccessResource : Endpoint() {
+
+    private var client: RepositoryClient<Any> = RepositoryClient()
+
+    @Path("assign${Endpoint.URI_PATH_SEPARATOR}${SubscriptionResource.NOTIFICATION_PATH_SUFFIX}")
+    @GET
+    fun assign(
+            request: Any?,
+            @Context requestContext: ContainerRequestContext,
+            @PathParam("url") url: String?,
+            @PathParam("eventUrl") eventUrl: String?,
+            @PathParam("token") token: String?,
+            @Suspended asyncResponse: AsyncResponse): Response {
+        println("assign = $request")
+
+
+        return Response.status(Response.Status.OK).type(MediaType.APPLICATION_ATOM_XML_TYPE).build()
+    }
+
+    @Path("unassign${Endpoint.URI_PATH_SEPARATOR}${SubscriptionResource.NOTIFICATION_PATH_SUFFIX}")
+    @GET
+    fun unassign(request: Any?,
+                 @Context requestContext: ContainerRequestContext,
+                 @PathParam("url") url: String?,
+                 @PathParam("eventUrl") eventUrl: String?,
+                 @PathParam("token") token: String?,
+                 @Suspended asyncResponse: AsyncResponse): Response {
+
+        Endpoint.LOG.info("unassign")
+
+        return Response.status(Response.Status.CREATED).type(MediaType.APPLICATION_ATOM_XML_TYPE).build()
+    }
+
+
+    companion object {
+        val RESOURCE_PATH = "appdirect/user"
+    }
+}
