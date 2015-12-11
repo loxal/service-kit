17b264cec6fd15cc708625cbd2f18314adcb5d8e
Alex <alexander.orlov@loxal.net>
2015-12-11 01:25:00 +0100

    remove downloading tomcat from default release flow, update release script

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Switch to Java 8 JDK
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+# export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
-mvn clean package cf:push -P cf-aws-stage # CloudFoundry
+# mvn clean package cf:push -P cf-aws-stage # CloudFoundry
 # mvn clean package appengine:update # GAE (managed)
-# export DOCKER_TLS_VERIFY=0
-# mvn clean validate gcloud:deploy # gcloud (managed / unmanaged)
+export DOCKER_TLS_VERIFY=0
+mvn clean validate gcloud:deploy # gcloud (managed / unmanaged)
