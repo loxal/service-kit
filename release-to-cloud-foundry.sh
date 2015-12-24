1541ca39306a1aa3663c1a692c6dd7c8b9ef0a54
Alex <alexander.orlov@loxal.net>
2015-12-24 21:59:42 +0100

    add additional deployment scripts for several cloud environments

--- release-to-cloud-foundry.sh
+++ release-to-cloud-foundry.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Switch to Java 8 JDK
+# Switch Java JDK
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
 mvn clean package cf:push -P cf-aws-stage
--- release-to-google-app-engine.sh
+++ release-to-google-app-engine.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Switch to Java 7 JDK
+# Switch Java JDK
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
 
 mvn clean package appengine:update
