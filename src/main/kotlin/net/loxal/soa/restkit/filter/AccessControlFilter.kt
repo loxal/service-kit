e63fb16190be94e26b98a1403f6779a1150b5ede
Alex <alexander.orlov@loxal.net>
2015-12-31 03:36:04 +0100

    adjust to work with an Angular2 client

--- src/main/kotlin/net/loxal/soa/restkit/filter/AccessControlFilter.kt
+++ src/main/kotlin/net/loxal/soa/restkit/filter/AccessControlFilter.kt
@@ -9,6 +9,7 @@ import javax.ws.rs.Priorities
 import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.container.ContainerResponseContext
 import javax.ws.rs.container.ContainerResponseFilter
+import javax.ws.rs.core.HttpHeaders
 import javax.ws.rs.ext.Provider
 import kotlin.collections.listOf
 
@@ -28,6 +29,6 @@ class AccessControlFilter : ContainerResponseFilter {
         const val allowOriginHeader = "Access-Control-Allow-Origin"
         const val allowOriginHeaderValue = "*"
         const val allowHeadersHeader = "Access-Control-Allow-Headers"
-        val allowHeadersValue = listOf("origin", "x-requested-with")
+        val allowHeadersValue = listOf("x-requested-with", HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT)
     }
 }
