90faa789e8c89d7a472b491753d384fa3c0e58cc
Alex <alexander.orlov@loxal.net>
2016-03-22 03:43:44 +0100

    update quotes; adjust & align names;

--- release-to-google-container-engine.sh
+++ release-to-google-container-engine.sh
@@ -3,12 +3,12 @@
 # Switch Java JDK
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
-gcloud config set project rest-kit-loxal
+gcloud config set project service-kit-loxal
 mvn clean validate gcloud:stage
 
 eval "$(docker-machine env default)"
-docker build --tag=gcr.io/rest-kit-loxal/default-docker-container .
-gcloud docker push gcr.io/rest-kit-loxal/default-docker-container
+docker build --tag=gcr.io/service-kit-loxal/default-docker-container .
+gcloud docker push gcr.io/service-kit-loxal/default-docker-container
 
 gcloud container clusters create docker-container-cluster \
     --scopes https://www.googleapis.com/auth/userinfo.email,\
@@ -25,11 +25,12 @@ https://www.googleapis.com/auth/cloud-platform,\
 https://www.googleapis.com/auth/bigtable.data,\
 https://www.googleapis.com/auth/bigtable.admin,\
 https://www.googleapis.com/auth/pubsub,\
-https://www.googleapis.com/auth/logging.write\
-    --num-nodes 3 \
+https://www.googleapis.com/auth/logging.write,\
+    --num-nodes 1 \
+    --image projects/coreos-cloud/global/images/coreos-stable-835-13-0-v20160218 \
     --machine-type g1-small
 
-kubectl run default-docker-container --image=gcr.io/rest-kit-loxal/default-docker-container
+kubectl run default-docker-container --image=gcr.io/service-kit-loxal/default-docker-container
 kubectl expose replicationcontrollers default-docker-container --type=LoadBalancer --port=80 --target-port=8080
 
 kubectl get services
