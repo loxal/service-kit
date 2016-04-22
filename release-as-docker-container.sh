e5624b47de1ad678ffeb9f4a69fe4144ce3afbf3
Alex <alexander.orlov@loxal.net>
2016-04-23 01:40:42 +0200

    update docker release script

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -6,8 +6,8 @@
 
 mvn clean package
 
-#docker login
-sudo docker build --tag=loxal/service-kit:v1 .
-sudo docker push loxal/service-kit:v1
-sudo docker rm -f service-kit
-sudo docker run -d -p 80:8080 --name service-kit loxal/service-kit:v1
+# docker login -u loxal
+docker build --tag=loxal/service-kit:v1 .
+docker push loxal/service-kit:v1
+docker rm -f service-kit
+docker run -d -p 80:8080 --name service-kit loxal/service-kit:v1
