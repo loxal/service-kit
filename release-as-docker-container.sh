fcc7bca64ed722d21eaece244a22793d0f408f5e
Alex <alexander.orlov@loxal.net>
2016-10-10 19:15:21 +0200

    add support badge

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -11,3 +11,5 @@ docker build --tag=loxal/service-kit:v1 .
 docker push loxal/service-kit:v1
 docker rm -f service-kit
 docker run -d -p 80:8080 --name service-kit loxal/service-kit:v1
+
+docker rmi $(docker images -f "dangling=true" -q) # cleanup, GC for dangling images
